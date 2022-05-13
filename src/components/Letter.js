9116c4dd39c267c0a307ae66936919936cd33f91
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-05-12 23:11:32 -0400

    keyboard works

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,10 +2,9 @@ import React, {useContext} from 'react'
 import { AppContext } from '../App'
 
 function Letter({letterPos, attemptVal}) {
-
-    const {board} = useContext(AppContext)
-    const letter = board[[attemptVal][letterPos]]
-
+    const { board } = useContext(AppContext)
+    console.log(board)
+    const letter = board[attemptVal][letterPos]
   return (
     <div className='letter'>{letter}</div>
   )
