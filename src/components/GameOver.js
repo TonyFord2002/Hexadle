e3d0d3e5db0054c58249a385ed3b5682a9e5dd61
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-05-14 17:18:53 -0400

    adding words

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -8,7 +8,7 @@ function GameOver() {
     <div className='gameOver'>
         <h2>{gameOver.guessedWord ? "You found the correct word!": "You did not find the correct word!"}</h2>
         <h1>Correct Word: {correctWord.toUpperCase()}</h1>
-        {gameOver.guessedWord && (<h3> You guess the word in {currAttempt.attempt} attempts</h3>)}
+        {gameOver.guessedWord && (<h3> You guessed the word in {currAttempt.attempt} attempts!</h3>)}
     </div>
   )
 }
