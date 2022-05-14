7c001c7bdd88db9f2d357354a6da645b5077fffb
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-05-14 13:52:57 -0400

    pages not working

--- src/Words.js
+++ src/Words.js
@@ -15,7 +15,7 @@ export const boardDefault = [
 
     await fetch(wordBank).then((res)=> res.text())
     .then((result)=>{
-      const wordArr = result.split(' ')
+      const wordArr = result.split('\n').slice(-2)
       todaysWord = wordArr[Math.floor(Math.random() *  wordArr.length)]
       wordSet = new Set(wordArr)
       console.log(wordSet,todaysWord)
